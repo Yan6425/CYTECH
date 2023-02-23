3cd50ef0466b86bc0b09aa0b7d6f4e353cbd2e23
Hourdebaigt--Hiroux Yan <hourdebaig@pce10620.etude.pau.eisti.fr>
2023-02-23 15:48:33 +0100

    0.1 puissance 4

--- puissance4/fonctions.h
+++ puissance4/fonctions.h
@@ -1,4 +1,16 @@
 #ifndef __puissance4_H_
 #define __puissance4_H_
-/* toutes mes signatures/prototypes de fonctions/procédures */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Joueur Joueur;
+char** creerGrille(int hauteur, int largeur);
+Joueur creerJoueur(int nJoueur);
+Joueur* creerTblJoueurs();
+int quiCommence();
+void afficherGrille(char** grille);
+void jouer(tblJoueur[nJoueur], grille);
+Joueur quiAGagne(char** grille, Joueur* tblJoueur);
+
 #endif
--- puissance4/main.c
+++ puissance4/main.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
 #include "fonctions.h"
 #define HAUTEUR = 6
 #define LARGEUR = 7
 
 int main(){
-    char** grille = malloc(HAUTEUR * sizeof(char*));
-    Joueur* tblJoueur = malloc(2 * sizeof(Joueur));
-    tblJoueur[0] = creerJoueur(1);
-    tblJoueur[1] = creerJoueur(2);
+    char** grille = creerGrille(HAUTEUR, LARGEUR);
+    Joueur* tblJoueur = creerTblJoueurs();
     Joueur gagnant;
-    srand(time(NULL));
-    int nJoueur = quiChoisi(tblJoueur[0], tblJoueur[1]);
-    choixSymbole(tblJoueur, nJoueur);
-    nJoueur = tblJoueur[0].symbole == 'O';
+    int nJoueur = quiCommence();
 
     do{
         afficherGrille(grille);
         jouer(tblJoueur[nJoueur], grille);
-        gagnant = quiAGagne(grille, tblJoueur[0], tblJoueur[1]);
+        gagnant = quiAGagne(grille, tblJoueur);
         nJoueur = !nJoueur;
     } while (!gagnant.existe);
     
